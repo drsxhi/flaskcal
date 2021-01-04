e1cd0a8349781be8af5d40b3ba84cdd8d0b08be3
Drshika Asher <drshika@Drshikas-MBP.MSRM>
2021-01-04 17:53:34 -0600

    added css and connected with google calendar apis

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, request, render_template
+from  __future__  import print_function
+from flask import Flask, render_template
+import datetime
+import pickle
+import os.path
+import googleapiclient.discovery
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 app = Flask(__name__)
 
@@ -8,6 +15,39 @@ def home():
 
 @app.route("/calendar")
 def calendar():
+    
+    creds =  None
+    SCOPES  = ['https://www.googleapis.com/auth/calendar.readonly']
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with  open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+    'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            # with open('token.pickle', 'wb') as token: # can't write files in Google App Engine so comment out or delete
+            # pickle.dump(creds, token)
+    service = googleapiclient.discovery.build('calendar', 'v3', credentials=creds)
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() +  'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    events_result = service.events().list(calendarId=ht3jlfaac5lfd6263ulfh4tql8@group.calendar.google.com, timeMin=now, maxResults=10, singleEvents=True, orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    if not events:
+        print('No upcoming events found.')
+    # for event in events:
+    # start = event['start'].get('dateTime', event['start'].get('date'))
+    # print(start, event['summary'])
+    event_list = [event["summary"] for event in events]
+
     return render_template("calendar.html")
     
 if __name__ == "__main__":
